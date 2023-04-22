7c88d8cac837e3f802477f67843050ffa4d3f8dd
garycor260176 <ipolnikov@gmail.com>
2023-04-22 21:25:45 +0300

    lesson5

--- src/com/urise/webapp/MainArray.java
+++ src/com/urise/webapp/MainArray.java
@@ -37,7 +37,7 @@ public class MainArray {
                     System.out.println(ARRAY_STORAGE.size());
                     break;
                 case "save":
-                    r = new Resume(uuid);
+                    r = new Resume(uuid, "name of " + uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
