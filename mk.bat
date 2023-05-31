11c52ae17671761fcd908c52e56b31d28b09e3b2
garycor260176 <ipolnikov@gmail.com>
2023-06-01 02:17:52 +0300

    lesson 9

--- mk.bat
+++
@@ -1,7 +0,0 @@
-SET filelist=%temp%\filelist-%random%.txt
-
-dir /s /b src\*.java > %filelist%
-
-javac @%filelist% -d out -encoding UTF-8
-
-del %filelist%
--- pushTest.bat
+++
@@ -1,3 +0,0 @@
-git add Test
-git commit -m "lesson8"
-git push -u origin master
