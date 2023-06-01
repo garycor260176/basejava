2b390c745e24e16481ffa30a65d69440dc6c65ae
garycor260176 <ipolnikov@gmail.com>
2023-06-01 23:14:50 +0300

    lesson 9

---
+++ Test/com/urise/webapp/storage/PathStorageTest.java
@@ -0,0 +1,10 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.storage.serializer.ObjectStreamSerializer;
+
+public class PathStorageTest extends AbstractStorageTest {
+
+    public PathStorageTest() {
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new ObjectStreamSerializer()));
+    }
+}
