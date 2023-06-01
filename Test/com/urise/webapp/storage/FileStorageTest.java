97370bdd9338beed2a4a4e7f1e9b516537f36ae0
garycor260176 <ipolnikov@gmail.com>
2023-06-01 23:10:54 +0300

    lesson 9

---
+++ Test/com/urise/webapp/storage/FileStorageTest.java
@@ -0,0 +1,10 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.storage.serializer.ObjectStreamSerializer;
+
+public class FileStorageTest extends AbstractStorageTest {
+
+    public FileStorageTest() {
+        super(new FileStorage(STORAGE_DIR, new ObjectStreamSerializer()));
+    }
+}
