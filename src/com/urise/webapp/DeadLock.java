7b29b1f2467050006bdcfda5c49bf4ac41ded628
garycor260176 <ipolnikov@gmail.com>
2023-06-07 21:41:00 +0300

    Merge branch 'hw_11'

--- src/com/urise/webapp/DeadLock.java
+++ src/com/urise/webapp/DeadLock.java
@@ -1,8 +1,8 @@
 package com.urise.webapp;
 
 public class DeadLock {
-    private static final String lock1 = "Поток №1";
-    private static final String lock2 = "Поток №2";
+    private static final String lock1 = "Thread №1";
+    private static final String lock2 = "Thread №2";
 
     public static void main(String[] args) {
         doDeadLock(lock1, lock2);
@@ -11,17 +11,17 @@ public class DeadLock {
 
     private static void doDeadLock(String lock1, String lock2) {
         new Thread(() -> {
-            System.out.println(lock1 + ": ожидание");
+            System.out.println(lock1 + ": waiting");
             synchronized (lock1) {
-                System.out.println(lock1 + ": выполнение");
+                System.out.println(lock1 + ": working");
                 try {
                     Thread.sleep(10);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                System.out.println(lock2 + ": ожидание");
+                System.out.println(lock2 + ": waiting");
                 synchronized (lock2) {
-                    System.out.println(lock2 + ": выполнение");
+                    System.out.println(lock2 + ": working");
                 }
             }
         }).start();
