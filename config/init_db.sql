63166eb8990b5784324da7e31d6de98dc6075379
garycor260176 <ipolnikov@gmail.com>
2023-06-14 10:44:56 +0300

    Merge branch 'hw_13'

--- config/init_db.sql
+++ config/init_db.sql
@@ -15,13 +15,3 @@ create table contact
 );
 create unique index contact_uuid_type_index
     on contact (resume_uuid, type);
-
-create table section
-(
-    id          serial,
-    resume_uuid char(36) not null references resume (uuid) on delete cascade,
-    type        text     not null,
-    content     text     not null
-);
-create unique index section_uuid_type_index
-    on section (resume_uuid, type);
